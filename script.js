34c986ba6a7b035f47e7316c90c8efb769b89991
GopalLohar <gopalloharnew@gmail.com>
2022-03-06 14:44:05 +0530

    removed search

--- script.js
+++ script.js
@@ -1,54 +1,45 @@
 const cardArray = [
-    {
-        cardTitle: "Title",
-        cardLink: "#",
-        cardTags: ["HTML", "CSS"],
-        cardDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, commodi.",
-        card: null
-    },
-    {
-        cardTitle: "New Card",
-        cardLink: "https://youtube.com",
-        cardTags: ["CSS"],
-        cardDescription: "Another Card",
-        card: null
-    },
-]
+  {
+    cardTitle: "Title",
+    cardLink: "#",
+    cardTags: ["HTML", "CSS"],
+    cardDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, commodi.",
+    card: null,
+    isFiltered: true,
+  },
+  {
+    cardTitle: "New Card",
+    cardLink: "https://youtube.com",
+    cardTags: ["CSS"],
+    cardDescription: "Another Card",
+    card: null,
+    isFiltered: true,
+  },
+];
 
 const search = document.querySelector("#search");
 const mycardTemplet = document.querySelector(".mycard-templet");
 const cardContainer = document.querySelector(".card-container");
+const searchTagWraper = [...document.querySelectorAll(".search-tag-wraper")];
 
 for (let i = 0; i < cardArray.length; i++) {
-    const cardInfo = cardArray[i];
+  const cardInfo = cardArray[i];
 
-    // creating card with cardArray
-    const cardTemplet = mycardTemplet.content.cloneNode(true).children[0];
-    cardTemplet.querySelector(".card-title").textContent = cardInfo.cardTitle;
-    cardTemplet.querySelector(".card-title").href = cardInfo.cardLink;
-    for (const cardTag of cardInfo.cardTags) {
-        let cardTagLi = document.createElement("li")
-        cardTagLi.classList.add("card-tag", "flex", "center")
-        cardTagLi.innerHTML = cardTag;
-        cardTemplet.querySelector(".card-tag-box").append(cardTagLi);
-    }
-    cardTemplet.querySelector(".card-description").textContent = cardInfo.cardDescription;
-    cardTemplet.querySelector(".card-link").href = cardInfo.cardLink;
+  // creating card with cardArray
+  const cardTemplet = mycardTemplet.content.cloneNode(true).children[0];
+  cardTemplet.querySelector(".card-title").textContent = cardInfo.cardTitle;
+  cardTemplet.querySelector(".card-title").href = cardInfo.cardLink;
+  for (const cardTag of cardInfo.cardTags) {
+    let cardTagLi = document.createElement("li");
+    cardTagLi.classList.add("card-tag", "flex", "center");
+    cardTagLi.innerHTML = cardTag;
+    cardTemplet.querySelector(".card-tag-box").append(cardTagLi);
+  }
+  cardTemplet.querySelector(".card-description").textContent =
+    cardInfo.cardDescription;
+  cardTemplet.querySelector(".card-link").href = cardInfo.cardLink;
 
-    cardContainer.append(cardTemplet);
-    cardInfo.card = cardTemplet;
+  cardContainer.append(cardTemplet);
+  cardInfo.card = cardTemplet;
 }
-
-
-search.addEventListener("input", ()=>{
-    let inputVal = search.value.trim().toLowerCase();
-    for (let j = 0; j < cardArray.length; j++) {
-        const cardInfo = cardArray[j];
-        let isMatch = cardInfo.cardTitle.trim().toLocaleLowerCase().includes(inputVal)||
-            cardInfo.cardDescription.trim().toLocaleLowerCase().includes(inputVal);
-        cardInfo.card.classList.toggle("hide", !isMatch);
-            
-       
-    }
-})
-
